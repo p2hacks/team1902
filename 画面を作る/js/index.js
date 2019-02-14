78c3f5c3ad3928284208f4131be34a62b5b3b5ff
Tom1363 <b1017055@fun.ac.jp>
2019-02-14 18:00:44 +0900

    ログイン画面、認証関連のJavaScript

--- 画面を作る/js/index.js
+++ 画面を作る/js/index.js
@@ -3,5 +3,7 @@ window.addEventListener("load", function(event) {
 	// - 認証済みならユーザページ
 	// - 未認証、認証切れならログインページ
 	
-	//location.href = "./index.html";
+	//if(認証済み) location.href = "./userPage.html";
+	
+	location.href = "./loginSelect.html";
 });
--- 画面を作る/js/main.js
+++ 画面を作る/js/main.js
@@ -1,12 +1,40 @@
+/*
+// ログインしていなかったらindex.htmlに返す
+var fname = window.location.href.split('/').pop();
+var flist = [	"index.html",
+				"login.html",
+				"loginSelect.html",
+				"userPage.html",
+				"createAccount.html"];	// この中にあるファイル名のときは実行されない（index.htmlを消すと無限ループになるので注意）
+if(flist.indexOf(fname) == -1){
+	// キーがなかったらエラーを吐くのでなかったらnullを入れておく
+	if(!("guestID" in localStorage)) {
+		var obj_guestID = {
+			"userID": null,
+			"SessionID": null
+		}
+		// ローカルストレージにnullのデータを追加
+		localStorage.setItem("guestID", JSON.stringify(obj_guestID));
+	}
+
+	// local Storageからデータを取りに行く
+	var SessionID = JSON.parse(localStorage.getItem("guestID"))["SessionID"];
+	
+	// ログインしていないなら
+	if(SessionID == null) {
+		console.log("ログインしていないのでindex.htmlに返します");
+		location.href="./index.html";
+	}
+}
+/**/
+
 // 認証関連のこと（毎更新時にやっていいものなのか？）
 window.addEventListener("load", function(event) {
-	console.log("ログイン情報が有効化チェックする関数を実行");
-	
 	// 認証済みか（SessionIDがあるかチェック）
 	if(!("guestID" in localStorage)) {
 		var obj_guestID = {
-			"userID":null,
-			"SessionID":null
+			"userID": null,
+			"SessionID": null
 		}
 		localStorage.setItem("guestID", JSON.stringify(obj_guestID));
 	}
@@ -21,4 +49,3 @@ window.addEventListener("load", function(event) {
 		});
 	}
 });
-
